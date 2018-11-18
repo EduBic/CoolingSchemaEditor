994c119a262a59239898ee01423ab1275773fc3c
EduBic <bic.studio@yahoo.it>
2018-11-18 16:36:02 +0100

    added coil

--- src/app/model/schema/GCoil.ts
+++ src/app/model/schema/GCoil.ts
@@ -1,36 +1,113 @@
 import * as SVG from 'svg.js';
 import { Point } from '../core/Point';
-import { HookPair } from '../core/HookPair';
-import { Direction } from '../core/Direction';
 import { HookPosition } from '../core/HookPosition';
 import { GElement } from './GElement';
 import { Gate } from './Gate';
 
+export enum GCoilPos {
+  Left, Right
+}
+
 export class GCoil extends GElement {
 
   private static readonly HOOK_POS_PERCENT = 0.2;
+  private static readonly ANGLE = 60 * Math.PI / 180; // radians
 
-  private height: number;
-  private depth: number;
+  private points: number[];
 
-  constructor(origin: Point, svgParent: SVG.G, height: number, depth: number) {
-    super(origin, svgParent, depth, height,
-      [
-        new Gate(new Point(GCoil.HOOK_POS_PERCENT * depth, height), HookPosition.Bottom, true),
-        new Gate(new Point((1 - GCoil.HOOK_POS_PERCENT) * depth, height), HookPosition.Bottom, false)
-      ]
-    );
+  constructor(origin: Point, svgParent: SVG.G, height: number, depth: number, pos: GCoilPos) {
+    if (pos === GCoilPos.Right) {
 
-    this.height = height;
-    this.depth = depth;
+      const baseBotTriangle = Math.abs(depth * Math.sin(GCoil.ANGLE));
+      const heightBotTriangle = Math.abs(depth * Math.cos(GCoil.ANGLE));
+
+      const baseTopTriangle = Math.abs(height * Math.sin(Math.PI / 2 - GCoil.ANGLE));
+      const heightTopTriangle = Math.abs(height * Math.cos(Math.PI / 2 - GCoil.ANGLE));
+
+      super(origin, svgParent, baseBotTriangle + baseTopTriangle, heightBotTriangle + heightTopTriangle,
+        [
+          GCoil.getEntryGate(baseBotTriangle, heightTopTriangle, heightBotTriangle, baseTopTriangle, pos),
+          GCoil.getExitGate(baseBotTriangle, heightBotTriangle, heightTopTriangle, baseTopTriangle, pos),
+        ]
+      );
+
+      // this.disableGateDraw();
+
+      // from bottom to left to top to right
+      this.points = [
+        baseBotTriangle, heightTopTriangle + heightBotTriangle, // A
+        0, heightTopTriangle, // B
+        baseTopTriangle, 0, // C
+        baseTopTriangle + baseBotTriangle, heightBotTriangle,  // D
+      ];
+
+    } else {
+      const baseBotTriangle = Math.abs(height * Math.sin(GCoil.ANGLE / 2));
+      const heightBotTriangle = Math.abs(height * Math.cos(GCoil.ANGLE / 2));
+
+      const baseTopTriangle = Math.abs(depth * Math.cos(GCoil.ANGLE / 2));
+      const heightTopTriangle = Math.abs(depth * Math.sin(GCoil.ANGLE / 2));
+
+      super(origin, svgParent, baseBotTriangle + baseTopTriangle, heightBotTriangle + heightTopTriangle,
+        [
+          GCoil.getEntryGate(baseBotTriangle, heightTopTriangle, heightBotTriangle, baseTopTriangle, pos),
+          GCoil.getExitGate(baseBotTriangle, heightBotTriangle, heightTopTriangle, baseTopTriangle, pos),
+        ]
+      );
+
+      // From bottom to right to top to left
+      this.points = [
+        // 0, 0, // origin
+        0, heightTopTriangle, // A
+        baseTopTriangle, 0, // B
+        baseTopTriangle + baseBotTriangle, heightBotTriangle, // C
+        baseBotTriangle, heightTopTriangle + heightBotTriangle // D
+      ];
+    }
   }
 
+  private static getEntryGate(baseBotTriangle, heightTopTriangle, heightBotTriangle, baseTopTriangle, pos: GCoilPos): Gate {
+    if (pos === GCoilPos.Right) {
+      return new Gate(
+        new Point(
+          GCoil.HOOK_POS_PERCENT * baseBotTriangle,
+          heightTopTriangle + GCoil.HOOK_POS_PERCENT * heightBotTriangle
+        ),
+        HookPosition.Bottom, true
+      );
+    } else {
+      return new Gate(
+        new Point(
+          baseTopTriangle + baseBotTriangle - GCoil.HOOK_POS_PERCENT * baseTopTriangle,
+          heightBotTriangle + GCoil.HOOK_POS_PERCENT * heightTopTriangle
+        ),
+        HookPosition.Bottom, true
+      );
+    }
+  }
+
+  private static getExitGate(baseBotTriangle, heightBotTriangle, heightTopTriangle, baseTopTriangle, pos: GCoilPos): Gate {
+    if (pos === GCoilPos.Right) {
+      return new Gate(
+        new Point(
+          baseBotTriangle - (GCoil.HOOK_POS_PERCENT) * baseBotTriangle,
+          (heightTopTriangle + heightBotTriangle) - (GCoil.HOOK_POS_PERCENT) * (heightBotTriangle)
+        ),
+        HookPosition.Bottom, false
+      );
+    } else {
+      return new Gate(
+        new Point(
+          baseBotTriangle + GCoil.HOOK_POS_PERCENT * baseTopTriangle,
+          (heightBotTriangle + heightTopTriangle) -  GCoil.HOOK_POS_PERCENT * baseTopTriangle
+        ),
+        HookPosition.Bottom, false
+      );
+    }
+  }
 
   public drawInternal(): void {
-    this.svgGroup.rect(this.depth, this.height);
-    this.svgGroup.rotate(35);
+    const rect = this.svgGroup.polygon(this.points);
   }
-
-
 
 }
