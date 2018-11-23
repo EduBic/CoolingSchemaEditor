b206e40c65f742e4d654e3ae6c76e710c140a055
EduBic <bic.studio@yahoo.it>
2018-11-23 15:39:07 +0100

    Rename Gelement panel for details

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PureSvgEditorComponent } from './pure-svg-editor/pure-svg-editor.component';
 import { StoreSelectionComponent } from './store-selection/store-selection.component';
 import { DragAndDropService } from './drag-and-drop.service';
-import { GElementPanelOptionsComponent } from './gelement-panel-options/gelement-panel-options.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StateSelectionService } from './state-selection.service';
 import { DataElementDetailsComponent } from './data-element-details/data-element-details.component';
+import { GraphicElementDetailsComponent } from './graphic-element-details/graphic-element-details.component';
 
 
 @NgModule({
   declarations: [
     PureSvgEditorComponent,
     StoreSelectionComponent,
-    GElementPanelOptionsComponent,
-    DataElementDetailsComponent
+    DataElementDetailsComponent,
+    GraphicElementDetailsComponent
   ],
   imports: [
     CommonModule,
