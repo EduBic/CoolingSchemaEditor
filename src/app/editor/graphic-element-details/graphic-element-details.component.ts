92a0b59baefba2b78057df8fdecb370f90763318
EduBic <bic.studio@yahoo.it>
2018-11-28 16:19:55 +0100

    Smooth scale with translate

--- src/app/editor/graphic-element-details/graphic-element-details.component.ts
+++ src/app/editor/graphic-element-details/graphic-element-details.component.ts
@@ -4,6 +4,7 @@ import { GElement } from '../model/schema/graphics/GElement';
 import { Subscription } from 'rxjs';
 import { StateSelectionService } from '../state-selection.service';
 import { Point } from '../model/core/Point';
+import { isNumber } from 'util';
 
 @Component({
   selector: 'app-graphic-element-details',
@@ -24,7 +25,7 @@ export class GraphicElementDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.selService.selectedGraphic$.subscribe((graphic: GElement) => {
-      // console.log('GElem panel before/after', this.graphicSelected, selection.getGraphic());
+      // console.log('GElem panel before/after', this.graphicSelected, graphic);
 
       if (this.subOriginX) {
         this.subOriginX.unsubscribe();
@@ -39,29 +40,31 @@ export class GraphicElementDetailsComponent implements OnInit {
       this.originY.setValue(this.graphicSelected.getOrigin().y);
 
       this.subOriginX = this.originX.valueChanges.subscribe((val: number) => {
-        if (this.isInputValid()) {
+        console.log('New OriginX', val);
+
+        if (this.isXinputValid()) {
           this.graphicSelected.setOrigin(new Point(val, this.originY.value));
         } else {
-          this.originX.setValue(0, {
-            emitEvent: false
-          });
+          this.originX.setValue(0, { emitEvent: false });
         }
       });
 
       this.subOriginY = this.originY.valueChanges.subscribe((val: number) => {
-        if (this.isInputValid()) {
+        if (this.isYInputValid()) {
           this.graphicSelected.setOrigin(new Point(this.originX.value, val));
         } else {
-          this.originY.setValue(0, {
-            emitEvent: false
-          });
+          this.originY.setValue(0, { emitEvent: false });
         }
       });
     });
   }
 
-  private isInputValid() {
-    return this.originX.value && this.originY.value;
+  private isXinputValid(): boolean {
+    return this.originX.value && isNumber(this.originX.value);
+  }
+
+  private isYInputValid() {
+    return this.originY.value && isNumber(this.originY.value);
   }
 
 }
